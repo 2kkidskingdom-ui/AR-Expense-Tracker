d9de431f46d3191a96eca4cf23a140808a16246f
2kkidskingdom-ui <2kkidskingdom@gmail.com>
2026-09-05 09:09:38 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ledger-v7';
+const CACHE = 'ledger-v8';
 const ASSETS = ['manifest.json','icon-192.png','icon-512.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS).catch(()=>{})).then(()=>self.skipWaiting()));
